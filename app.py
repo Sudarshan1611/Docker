6bd7281ae3c69adda5e43cd365e70b301bffa3c5
Sudarshan1611 <164621028+Sudarshan1611@users.noreply.github.com>
2024-03-30 09:29:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("hello world");
+from flask import flask
+app = flask(__name__)
+@app.route('/')
+def hello_world():
+        return 'Hello, World!'
+if __name__ == '__main__':
+        app.run(debug=True, host='5000')
